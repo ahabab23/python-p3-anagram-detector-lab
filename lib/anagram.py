1473f7d15fe805e699580fd6658499a544709d2f
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-26 15:25:34 +0300

    Anagram detectors

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+
+        return match_word_list
+    
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
